94f7bf0ca3e9c2543beb3cad9d6de6521b95a39d
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 17:30:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,7 @@ class Application
       item = req.params["q"]
 
       if @@items.include?(item)
+        @@cart << item
         resp.write "added #{item}"
       else
         resp.write "We don't have that item"

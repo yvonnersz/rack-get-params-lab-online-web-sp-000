2a2f41780d4d1e8ca426c7a324fdfb230a7769be
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 17:28:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
       @@cart.each do |item|
         resp.write "#{item}\n"
       end

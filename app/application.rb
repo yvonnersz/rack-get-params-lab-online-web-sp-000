c4334c6452f114912ba9b69069d6d7f99192b07a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 17:24:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
     else
       resp.write "Path Not Found"
     end

0ef7a966235ab617cd3868024133d1b7647f066b
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-13 17:28:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,16 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/add/)
+
+      item = req.params["q"]
+
+      if @@items.include?(item)
+        @@cart << item
+      else
+        resp.write "Error"
+      end
+      
     else
       resp.write "Path Not Found"
     end
